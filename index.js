897dd4b27f424b4bdb039089066520888625a645
hitesh12321 <sainihitesh1123@gmail.com>
2026-06-07 01:26:23 +0530

    preetier added

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
